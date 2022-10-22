4682b662ce14c55ce7f89826e764d965ed33fcc3
Feldspar <luis.m.sousa10@gmail.com>
2022-10-22 23:22:57 +0100

    main.cpp test

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,9 @@
 int main(){
     Lecture teste1("Teste","Monday",8,2,"Gay"), teste2("Teste", "Thursday",14.5,1,"Gay");
     Class_Hour testClass("Gay");
+    Student testStudent("Canadiano",202012345);
     testClass.lectures.push_back(teste1);
     testClass.lectures.push_back(teste2);
-    std::cout << Timetable(testClass) << std::endl;
+    testStudent.classes.push_back(&testClass);
+    std::cout << Timetable(testStudent) << std::endl;
 }
