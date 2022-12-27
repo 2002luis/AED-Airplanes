755bdb361c285939d427bcc75694b5e115fd9a0f
Feldspar <luis.m.sousa10@gmail.com>
2022-12-27 02:45:11 +0000

    Bruh

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,27 @@
 int main() {
     Graph testG;
     Airport a1("BRUH","","","",3,3),a2("BRUH2","","","",3,3),a3("BRUH3","","","",3,3),
-            a4("BRUH4","","","",3,3), a5("BRUH5","","","",3,3);
+            a4("BRUH4","","","",3,3), a5("BRUH5","","","",3,3),
+            a6("BRUH6","","","",3,3),a7("BRUH7","","","",3,3);
     testG.addAirport(a1);
     testG.addAirport(a2);
     testG.addAirport(a3);
     testG.addAirport(a4);
     testG.addAirport(a5);
+    testG.addAirport(a6);
+    testG.addAirport(a7);
+
     testG.addFlight("BRUH","BRUH2","gay");
+    testG.addFlight("BRUH","BRUH4","gay");
+
     testG.addFlight("BRUH2","BRUH3","gay");
     testG.addFlight("BRUH3","BRUH5","gay");
-    testG.addFlight("BRUH","BRUH5","gay");
+
+    testG.addFlight("BRUH4","BRUH7","gay");
+
+    testG.addFlight("BRUH5","BRUH7","gay");
+
+    testG.addFlight("BRUH6","BRUH5","gay");
 
     std::cout << testG;
 
